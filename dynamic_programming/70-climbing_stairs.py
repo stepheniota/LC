4302f23e18587bfbfb47a4de111c167608923647
stepheniota <iota@usc.edu>
2021-06-15 17:07:46 -0700

    optimizations

--- dynamic_programming/70-climbing_stairs.py
+++ dynamic_programming/70-climbing_stairs.py
@@ -5,24 +5,21 @@ You are climbing a staircase. It takes n steps to reach the top.
 Each time you can either climb 1 or 2 steps. 
 In how many distinct ways can you climb to the top?
 '''
-# _tags = {'dynamic programing'}
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # let opt[i] be the number of ways to arrive at at step i
-        # prepend an extra 0 for `0th` step
-        opt = [0 for i in range(n+1)]
-
-        # boundary conditions: 
-        opt[0], opt[1] = 1, 1 
-
-        # recursion relation: opt[i] = opt[i-1] + opt[i-2]
-        for i in range(2,n+1):
-            opt[i] = opt[i-1] + opt[i-2]
-
-        # the answer is stored in the last element
-        return opt[-1]
-
+        # let opt[i] be the number of ways to reach the ith step
+        # base: opt[0], opt[1] = 1, 1
+        # recursion relation: opt[i] = opt[i-2] + opt[i-1]
+        # let one be opt[i-1], let two be opt[i-1]
+        
+        opt, one, two = 1, 1, 1
+        
+        for i in range(2, n + 1):
+            opt = one + two
+            two, one = opt, two
+            
+        return opt
 
 if __name__ == '__main__':
     sol = Solution()
