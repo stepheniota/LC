a83ef9f29376a17095d60229ecd3c42859340ae5
stepheniota <iota@usc.edu>
2021-06-19 11:35:37 -0700

    add alt solution

--- linked_lists/141-ll_cycle.py
+++ linked_lists/141-ll_cycle.py
@@ -7,6 +7,7 @@ from linked_list import ListNode
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
+        '''hashset approach; O(n) time and space'''
         visited = set()
         curr = head
 
@@ -17,5 +18,19 @@ class Solution:
             curr = curr.next
 
         return False 
+
+    def hasCycle2(self, head: ListNode) -> bool:
+        '''2 ptr approach; O(n) time, O(1) space'''
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        
+        return False
+
+    
 
 
