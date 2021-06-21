4c5a82f8e9c02d8b4574eb6ac77db0f0c7c22d93
stepheniota <iota@usc.edu>
2021-06-20 18:08:06 -0700

    new solutions

---
+++ arrays/198-rotate_array.py
@@ -0,0 +1,16 @@
+'''
+leetcode 198 - rotate array
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+'''
+from typing import List
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n    # takes care of edge case k > n
+
+        nums[:k], nums[k:] = nums[n-k:], nums[:n-k]
+        return 
