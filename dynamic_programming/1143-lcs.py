067056ffca551f74ec1703c73d9fbdd53387c653
stepheniota <iota@usc.edu>
2021-06-26 22:47:16 -0700

    revisions

--- dynamic_programming/1143-lcs.py
+++ dynamic_programming/1143-lcs.py
@@ -6,19 +6,18 @@ If there is no common subsequence, return 0.
 '''
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # let opt[i][j] be optimal value for first i, j letters in text1, text2
-        # recursion equation: opt[i][j] = 1 + opt[i+1][j+1] if i,jth letter in 1,2 are equal
-        #                     opt[i][j] = max(diagonals otherwise
-        n1, n2 = len(text1), len(text2)
-        opt = [[0 for i in range(n1 + 1)] for j in range(n2 + 1)]
-        
-        for i in range(n2):
-            for j in range(n1):
-                if text1[j] == text2[i]:
-                    opt[i + 1][j + 1] = 1 + opt[i][j]
-                else:
-                    opt[i + 1][j + 1] = max(opt[i][j + 1], opt[i + 1][j])
-                
-        return opt[n2][n1]
+        '''let opt[i][j] be optimal value for text2[i:], text1[j:]
+        recurrence equation: opt[i][j] = 1 + opt[i+1][j+1] if text1[j] == text2[i]
+                             opt[i][j] = max(diagonals) otherwise
+        '''
+        m, n = len(text1), len(text2)
+        opt = [[0] * (m + 1) for i in range(n + 1)]
 
-        
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                if text1[j] == text2[i]:
+                    opt[i][j] = 1 + opt[i + 1][j + 1]
+                else:
+                    opt[i][j] = max(opt[i + 1][j], opt[i][j + 1])
+
+        return opt[0][0]
