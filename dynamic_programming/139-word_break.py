31ab6c91d4e15ca5d18eaebe17c59c90083863ba
stepheniota <iota@usc.edu>
2021-06-28 23:06:45 -0700

    clean up code

--- dynamic_programming/139-word_break.py
+++ dynamic_programming/139-word_break.py
@@ -8,20 +8,18 @@ sequence of one or more dictionary words.
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         '''O(n^2) time, O(n) space'''
-        n = len(s)
-        wordDict = set(wordDict)  # for faster word lookup
-        opt = [False] * (n + 1)
-        opt[-1] = True  # base case
+        wordDict = set(wordDict)
 
-        # let opt[i] be whether s[i:] can be wordbroken
-        # relation: opt[i] = True if opt[i:j] & opt[j:]
-        # for some j in [i, n]
+        # let opt[i] be True if s[i:] in wordDict
+        # opt[i] is true for s[i:j] if opt[j] is true
+        opt = [False] * len(s)
+        opt.append(True) # empty string base case
 
-        for i in range(n - 1, -1, -1):
-            for j in range(i, n):
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(i, len(s)):
                 if s[i:j + 1] in wordDict:
                     opt[i] = opt[j + 1]
                     if opt[i]:
-                        break  # if instead continue, risk throwing away result
+                        break  # risk throwing result away
 
         return opt[0]
